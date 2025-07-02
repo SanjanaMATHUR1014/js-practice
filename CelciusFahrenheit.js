782549fbc9b6a64478874a88b1e64e58a53b9001
SanjanaMATHUR1014 <14sanjanamathur@gmail.com>
2025-07-02 11:32:09 +0530

    mess

---
+++ CelciusFahrenheit.js
@@ -0,0 +1,8 @@
+let celsius= 25;
+let fahrenheit = (celsius * 9/5)+32;
+console.log(`${celsius}°C =${fahrenheit}°F`);
+
+/*    °F to °C
+fahrenheit= 77;
+celsius= (fahrenheit-32)*5/9;
+console.log((`${fahrenheit}°F =${celsius.toFixed(2)}°C`);
